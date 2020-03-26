8b4c1a27ad21a926c90b249b5452164158c27dbc
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2020-03-26 12:57:58 +0000

    Format

--- bot.py
+++ bot.py
@@ -10,9 +10,11 @@ RENAME_REGEX = re.compile(r"^[Ii]'m (\w+)$")
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
+
 
 @client.event
 async def on_message(message):
@@ -22,7 +24,7 @@ async def on_message(message):
     match = re.match(RENAME_REGEX, message.content)
     if match:
         name = match[1]
-        await message.channel.send(f'Hello {name}!')
+        await message.channel.send(f"Hello {name}!")
         await client.http.change_nickname(message.guild.id, message.author.id, name)
 
 
